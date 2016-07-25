16b6ae5b6dd40b5549e3dc42e25c088b9fc93c9d
lawenliu <lwc541117@gmail.com>
2016-07-24 22:00:23 -0500

    Change Ads library to wandoujia

--- AntiMosquito/app/src/main/java/com/care/antimosquito/MainActivity.java
+++ AntiMosquito/app/src/main/java/com/care/antimosquito/MainActivity.java
@@ -3,34 +3,33 @@ package com.care.antimosquito;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.care.core.Constants;
 import com.care.core.SharedDataManager;
 import com.care.core.Utilities;
 import com.flurry.android.FlurryAgent;
-import com.flurry.android.ads.FlurryAdBanner;
-import com.flurry.android.ads.FlurryAdBannerListener;
-import com.flurry.android.ads.FlurryAdErrorType;
-
-import java.io.IOException;
+import com.wandoujia.ads.sdk.Ads;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mModeDescriptionTextView = null;
     private ImageView mAntiMosquitoImageView = null;
     private ImageButton mSwitchImageButton = null;
-    private RelativeLayout mFlurryAdBannerLayout = null;
-
-    private FlurryAdBanner mFlurryAdBanner = null;
+    private LinearLayout mAdBannerLayout = null;
 
     private MediaPlayer mMediaPlayer1 = null;
     private MediaPlayer mMediaPlayer2 = null;
@@ -43,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         mModeDescriptionTextView = (TextView)this.findViewById(R.id.id_mode_description);
         mAntiMosquitoImageView = (ImageView)this.findViewById(R.id.id_anti_mosquito_image);
         mSwitchImageButton = (ImageButton)this.findViewById(R.id.id_anti_mosquito_switch);
-        mFlurryAdBannerLayout = (RelativeLayout)this.findViewById(R.id.id_flurry_ad_banner);
+        mAdBannerLayout = (LinearLayout)this.findViewById(R.id.id_ad_banner);
 
         mAntiMosquitoImageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,49 +71,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mFlurryAdBanner = new FlurryAdBanner(this, mFlurryAdBannerLayout, Constants.FlurryAdSpace);
-        mFlurryAdBanner.setListener(new FlurryAdBannerListener() {
-            @Override
-            public void onFetched(FlurryAdBanner flurryAdBanner) {
-                flurryAdBanner.displayAd();
-            }
-
-            @Override
-            public void onRendered(FlurryAdBanner flurryAdBanner) {
-            }
-
-            @Override
-            public void onShowFullscreen(FlurryAdBanner flurryAdBanner) {
-            }
-
-            @Override
-            public void onCloseFullscreen(FlurryAdBanner flurryAdBanner) {
-            }
-
-            @Override
-            public void onAppExit(FlurryAdBanner flurryAdBanner) {
-            }
-
-            @Override
-            public void onClicked(FlurryAdBanner flurryAdBanner) {
-            }
-
-            @Override
-            public void onVideoCompleted(FlurryAdBanner flurryAdBanner) {
-            }
-
-            @Override
-            public void onError(FlurryAdBanner flurryAdBanner, FlurryAdErrorType flurryAdErrorType, int i) {
-                flurryAdBanner.destroy();
-            }
-        });
-
         mMediaPlayer1 = MediaPlayer.create(this, R.raw.sound1);
         mMediaPlayer1.setLooping(false);
         mMediaPlayer2 = MediaPlayer.create(this, R.raw.sound2);
         mMediaPlayer2.setLooping(false);
 
         changeStatus();
+        initializeAds();
+    }
+
+    private void initializeAds() {
+        new AsyncTask<Void, Void, Boolean>() {
+            @Override
+            protected Boolean doInBackground(Void... params) {
+                try {
+                    Ads.init(MainActivity.this, Constants.WANDOUJIA_APP_ID, Constants.WANDOUJIA_SECRET_KEY);
+                    return true;
+                } catch (Exception e) {
+                    return false;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Boolean success) {
+                if (success) {
+                    Ads.preLoad(Constants.WANDOUJIA_BANNER_ID, Ads.AdFormat.banner);
+                    View bannerView = Ads.createBannerView(MainActivity.this, Constants.WANDOUJIA_BANNER_ID);
+                    mAdBannerLayout.addView(bannerView, new ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT
+                    ));
+
+                }
+            }
+        }.execute();
     }
 
     @Override
@@ -144,10 +134,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        if(mFlurryAdBanner != null) {
-            mFlurryAdBanner.destroy();
-        }
-
         super.onDestroy();
     }
 
@@ -156,14 +142,13 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
 
         FlurryAgent.onStartSession(this);
-        mFlurryAdBanner.fetchAd();
     }
 
     @Override
     public void onStop() {
-        super.onStop();
-
         FlurryAgent.onEndSession(this);
+
+        super.onStop();
     }
 
     public void changeStatus() {
--- AntiMosquito/app/src/main/java/com/care/core/Constants.java
+++ AntiMosquito/app/src/main/java/com/care/core/Constants.java
@@ -12,7 +12,10 @@ public class Constants {
     public static String SharedPreferenceFileName = "care_anti_mosquito";
 
     public static String FlurryApplicationKey = "DCCDGQKMRCC5K84WP936";
-    public static String FlurryAdSpace = "AntiMosquito_Banner";
+    public static String WANDOUJIA_APP_ID = "100041934";
+    public static String WANDOUJIA_SECRET_KEY = "7cd9d22a78f1e0b689735537e117388a";
+    public static String WANDOUJIA_BANNER_ID = "169c1f7ed62727d0d86c8e2295d22d5f";
+
 
     // Key for shared data
     public static String KeyAntiMosquitoTurnedOn = "AntiMosquitoTurnedOn";
